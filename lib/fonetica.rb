d6b22f1853cc62f3e9cd2e3838e0d40bf20ef0b8
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2010-10-18 09:50:02 -0200

    Require railtie when in rails
    This reverts part of commit ec3e381de01a20dcd18561079cc430d87477a5c0

--- lib/fonetica.rb
+++ lib/fonetica.rb
@@ -1,4 +1,5 @@
 require 'fonetica/core_ext/string'
+require 'fonetica/railtie' if defined?(Rails)
 
 module Fonetica
   autoload :ActiveRecord, 'fonetica/active_record'
--- lib/fonetica/railtie.rb
+++ lib/fonetica/railtie.rb
@@ -1,8 +1,7 @@
 module Fonetica
   class Railtie < Rails::Railtie
     initializer 'fonetica.active_record' do
-      ActiveSupport.on_load :active_record do
-        require 'fonetica/active_record'
+      ActiveSupport.on_load(:active_record) do
         include Fonetica::ActiveRecord
       end
     end
