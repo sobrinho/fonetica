ab7eb0df126e78fff7dd09aebd9b221f2d8420bc
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2010-10-18 09:43:34 -0200

    Specs for active record

--- fonetica.gemspec
+++ fonetica.gemspec
@@ -16,5 +16,7 @@ Gem::Specification.new do |s|
   s.require_paths = ["lib"]
   s.add_dependency(%q<activesupport>, [">= 3.0.0"])
   s.add_dependency(%q<activerecord>, [">= 3.0.0"])
-  s.add_development_dependency(%q<rspec>, ["~> 2.0.0"])
+  s.add_development_dependency(%q<railties>, [">= 3.0.0"])
+  s.add_development_dependency(%q<rspec>, [">= 2.0.0"])
+  s.add_development_dependency(%q<sqlite3-ruby>, [">= 1.3.1"])
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,22 @@
 require 'rubygems'
-require 'active_support'
+require 'bundler/setup'
+require 'active_record'
 require 'fonetica'
+require 'fonetica/active_record'
+
+ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database => ':memory:'
+ActiveRecord::Migration.verbose = false
+
+ActiveRecord::Schema.define(:version => 0) do
+  create_table :people do |t|
+    t.string :name
+    t.string :fonetica_name
+  end
+  
+  add_index :people, :fonetica_name
+end
+
+class Person < ActiveRecord::Base
+end
+
+ActiveRecord::Base.send(:include, Fonetica::ActiveRecord)
